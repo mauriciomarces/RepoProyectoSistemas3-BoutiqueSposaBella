e2b488267369cdfb40bca5189dd7cb84016ec9c7
mauriciomarces <mauriciomarces1@gmail.com>
2025-10-22 20:11:14 -0400

    Resolver conflicto en routes/web.php fusionando rutas de index y definitiva-Caleb

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\CatalogoController;
 use App\Http\Controllers\ClienteController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-=======
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\MovimientoController;
 
-Route::resource('categorias', CategoriaController::class);
-Route::resource('productos', ProductoController::class);
-Route::resource('movimientos', MovimientoController::class);
-
-
-Route::get('/productos/stock-bajo', [ProductoController::class, 'bajoStock'])->name('productos.bajoStock');
->>>>>>> origin/definitiva-Caleb
-
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
-//CATÁLOGO
-
+// RUTAS CATÁLOGO
 Route::get('/catalogo', [CatalogoController::class, 'index']);
 
-//CLIENTES
+// RUTAS CLIENTES
 Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
 Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
 Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
@@ -45,5 +22,9 @@ Route::get('/clientes/{id}', [ClienteController::class, 'show'])->name('clientes
 Route::get('/clientes/{id}/edit', [ClienteController::class, 'edit'])->name('clientes.edit');
 Route::put('/clientes/{id}', [ClienteController::class, 'update'])->name('clientes.update');
 Route::delete('/clientes/{id}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
-=======
->>>>>>> origin/definitiva-Caleb
+
+// RUTAS DEFINITIVA-CALEB
+Route::resource('categorias', CategoriaController::class);
+Route::resource('productos', ProductoController::class);
+Route::resource('movimientos', MovimientoController::class);
+Route::get('/productos/stock-bajo', [ProductoController::class, 'bajoStock'])->name('productos.bajoStock');
